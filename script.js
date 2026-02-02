4d4fb0098c14baffc25f43d72e285d802f0c3260
ProfNardella <m.nardella@isufol.it>
2026-02-02 15:36:52 +0100

    Refactor quiz loading and answer checking logic

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
-fetch("./data/invalsi1.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (domande) {
-    var quizDiv = document.getElementById("quiz");
+wwindow.onload = function () {
 
-    domande.forEach(function (domanda, index) {
-      var html = "<p><strong>" + (index + 1) + ". " + domanda.testo + "</strong></p>";
-
-      domanda.opzioni.forEach(function (opzione, i) {
-        html +=
-          '<label>' +
-          '<input type="radio" name="q' + index + '" value="' + i + '">' +
-          opzione +
-          "</label><br>";
-      });
-
-      quizDiv.innerHTML += html;
-    });
-  });
-
-function correggi() {
   fetch("./data/invalsi1.json")
     .then(function (response) {
       return response.json();
     })
     .then(function (domande) {
+
+      var quizDiv = document.getElementById("quiz");
+
+      for (var i = 0; i < domande.length; i++) {
+        var domanda = domande[i];
+
+        var p = document.createElement("p");
+        p.innerHTML = "<strong>" + (i + 1) + ". " + domanda.testo + "</strong>";
+        quizDiv.appendChild(p);
+
+        for (var j = 0; j < domanda.opzioni.length; j++) {
+          var label = document.createElement("label");
+
+          var input = document.createElement("input");
+          input.type = "radio";
+          input.name = "q" + i;
+          input.value = j;
+
+          label.appendChild(input);
+          label.appendChild(document.createTextNode(" " + domanda.opzioni[j]));
+
+          quizDiv.appendChild(label);
+          quizDiv.appendChild(document.createElement("br"));
+        }
+      }
+    });
+};
+
+function correggi() {
+
+  fetch("./data/invalsi1.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (domande) {
+
       var punteggio = 0;
 
-      domande.forEach(function (domanda, index) {
+      for (var i = 0; i < domande.length; i++) {
         var risposta = document.querySelector(
-          'input[name="q' + index + '"]:checked'
+          'input[name="q' + i + '"]:checked'
         );
 
-        if (risposta && parseInt(risposta.value) === domanda.corretta) {
+        if (risposta && parseInt(risposta.value, 10) === domande[i].corretta) {
           punteggio++;
         }
-      });
+      }
 
       document.getElementById("risultato").innerText =
         "Risultato: " + punteggio + " / " + domande.length;
