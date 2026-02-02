785967c2b4779e448baf1df56f23232ebeef25ae
ProfNardella <m.nardella@isufol.it>
2026-02-02 18:10:52 +0100

    Implement response evaluation and result display
    Fetches data from a JSON file and evaluates user responses, updating the UI with results.

--- script.js
+++ script.js
@@ -41,8 +41,62 @@ fetch("./data/invalsi1.json")
 
 // FUNZIONI GLOBALI
 function correggi() {
-  alert("Correzione OK");
+  fetch("./data/invalsi1.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (domande) {
+
+      var punteggio = 0;
+
+      for (var i = 0; i < domande.length; i++) {
+
+        var rispostaSelezionata = document.querySelector(
+          'input[name="q' + i + '"]:checked'
+        );
+
+        var opzioni = document.querySelectorAll(
+          'input[name="q' + i + '"]'
+        );
+
+        for (var j = 0; j < opzioni.length; j++) {
+          var label = opzioni[j].parentElement;
+
+          // pulizia classi precedenti
+          label.classList.remove("opzione-corretta", "opzione-sbagliata");
+
+          // evidenzia corretta
+          if (j === domande[i].corretta) {
+            label.classList.add("opzione-corretta");
+          }
+
+          // evidenzia sbagliata selezionata
+          if (rispostaSelezionata &&
+              j === parseInt(rispostaSelezionata.value, 10) &&
+              j !== domande[i].corretta) {
+            label.classList.add("opzione-sbagliata");
+          }
+        }
+
+        if (rispostaSelezionata &&
+            parseInt(rispostaSelezionata.value, 10) === domande[i].corretta) {
+          punteggio++;
+        }
+      }
+
+      // mostra risultato
+      var risultato = document.getElementById("risultato");
+      if (!risultato) {
+        risultato = document.createElement("div");
+        risultato.id = "risultato";
+        document.body.appendChild(risultato);
+      }
+      risultato.className = "risultato";
+      risultato.innerText =
+        "Risposte corrette: " + punteggio + " su " + domande.length;
+    });
 }
+
 
 function scaricaPDF() {
   alert("PDF OK");
